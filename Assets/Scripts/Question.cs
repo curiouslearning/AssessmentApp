0d2d05e8ca907604931cc2afc7986ef2ab4cdfc3
Galen March <gamarch@vassar.edu>
2015-06-17 11:51:53 -0400

    Prefabs first commit

---
+++ Assets/Scripts/Question.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Question : MonoBehaviour {
+
+	private int questionNumber;
+	private SOOScript stimuli;
+	private StimulusScript correctAnswer;
+
+
+	// Accessing variables
+
+	public int returnQuestionNumber() {
+		return questionNumber;
+	}
+
+	public SOOScript returnStimuli() {
+		return stimuli;
+	}
+
+	public StimulusScript returnStimulusScript() {
+		return correctAnswer;
+	}
+
+	// Setting variables
+
+	public void setQuestionNumber(int input) {
+		questionNumber = input;
+	}
+
+	public void setStimuli(SOOScript input) {
+		stimuli = input;
+	}
+
+	public void setCorrectAnswer(StimulusScript input) {
+		correctAnswer = input;
+	}
+
+
+	void Start () {
+		this.questionNumber = questionNumber;
+		this.stimuli = stimuli;
+		this.correctAnswer = correctAnswer;
+	}
+	
+
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/StimulusScript.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StimulusScript : MonoBehaviour {
+
+	private bool isCorrect;
+	private bool isBeingDragged; 
+	private bool isDraggable;
+	private Transform curPos;
+	private Vector3 size;
+
+	// Methods for accessing variables
+
+	public bool returnIsCorrect() {
+		return isCorrect;
+	}
+
+	public bool returnIsBeingDragged() {
+		return isBeingDragged;
+	}
+
+	public bool returnIsDraggable() {
+		return isDraggable;
+	}
+
+	public Transform returnCurPos() {
+		return curPos;  
+	}
+
+	public Vector3 returnSize() {
+		return curPos.lossyScale;
+	}
+
+	// Methods for setting variables
+
+	public void setIsCorrect(bool input) {
+		isCorrect = input;
+	}
+
+	public void setIsBeingDragged(bool input) {
+		isBeingDragged = input;
+	}
+
+	public void setIsDraggable(bool input) {
+		isDraggable = input;
+	}
+
+	public void setCurPos(Transform input) {
+		curPos = input;
+	}
+
+	// private Stimulus resize() {
+	// size = size * distanceToHost/someConstant
+	// }
+
+	void Start () {
+		this.isCorrect = isCorrect;
+		this.isBeingDragged = false;
+		this.isDraggable = true;
+		this.curPos = curPos;
+		this.size = curPos.lossyScale;
+	}
+	
+
+	void Update () {
+
+	}
+}
