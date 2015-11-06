4dd0bace8a0df84c60dd4bcb3d413869f5780e5a
Eric <bookjunkie93@gmail.com>
2015-11-06 11:06:15 -0500

    refactored findStim completely

--- Assets/Scripts/SpawnerScript.cs
+++ Assets/Scripts/SpawnerScript.cs
@@ -2,7 +2,6 @@
 using System.Collections;
 using System.Collections.Generic;
 
-
 /// <summary>
 /// SpawnerScript
 /// Class used to initialize Stimulus Organization Object (SOO) and stimuli for each new question.
@@ -11,15 +10,21 @@ using System.Collections.Generic;
 // Gets SOO and stimulus positioning info from Editor.
 public class SpawnerScript : MonoBehaviour {
 	GameObject newSoo;
+	//variables to hold stimulus selection components
 	GameObject[] newStims;
 	Sprite[] newOptions;
 	Dictionary<string, Difficulty> diffParser;
 	Dictionary<string, Category> catParser;
 	List<serStim> stimPool;
+	//initialization variables
 	public TextAsset stimList;
 	public GameObject sooPrefab;
 	public GameObject stimPrefab;
+	//components
 	public GameObject character;
+	public AnimationManager host;
+	public ToggleBasket receptacle;
+	public BackgroundScroller background;
 	//placement modifiers for stimulus positions
 	public Vector4 visStimSpacing;
 	public Vector4 charStimSpacing;
@@ -31,11 +36,10 @@ public class SpawnerScript : MonoBehaviour {
 	//variables for the SOO to hold onto
 	public Vector3[] destinations;
 	Vector3[] positions;
-	public AnimationManager host;
-	public ToggleBasket receptacle;
-	public BackgroundScroller background;
+	
 	// Use this for initialization
-	void Awake () {
+	void Awake () 
+	{
 		diffParser = new Dictionary<string, Difficulty>();
 		diffParser.Add ("Easy", Difficulty.Easy);
 		diffParser.Add ("Medium", Difficulty.Medium);
@@ -86,21 +90,7 @@ public class SpawnerScript : MonoBehaviour {
 	}
 
 
-	string setType (Category cat)
-	{
-		string type;
-		if (cat.Equals (Category.ReceptiveVocabulary) || cat.Equals(Category.Customization)) {
-			type = "visual";
-		} else {
-			type = "audio";
-		}	
-		return type;
-	}
-
-	/*serStim selectTarget (Category cat, Difficulty diffLevel, string type)
-	{
-		//TODO: MOVE THE FIND TARGET WHILE LOOP HERE
-	}*/
+	
 
 	/// <summary>
 	/// Returns a list of four semi-randomly generated serStims, one of which is
@@ -110,99 +100,127 @@ public class SpawnerScript : MonoBehaviour {
 	/// <param name="cat">current Category in game</param>
 	/// <param name="diffLevel">current Difficulty in game</param>
 
-	//TODO: REFACTOR THIS BEHEMOTH
-	List<serStim> findStim (Category cat, Difficulty diffLevel) {
+	List<serStim> findStim (Category cat, Difficulty diffLevel) 
+	{
 		List<serStim> answer = new List<serStim>();
 		int counter = 0;
-		int targetCount = 0;
-		int revolutionCount =0;
 		string type;
 		
 		// the type of stimulus accepted is assigned based on the current category
 		type = setType(cat);
-		while (answer.Count == 0)
+		//find and add the target stimulus
+		serStim temp = selectTarget(cat, diffLevel, type);
+		answer.Add (temp);
+		//find and add the remaining stimuli
+		while (answer.Count < 4) 
 		{
-			if (counter == stimPool.Count) //remember to declare this in new fxn
-				counter = 0;
-			serStim s = stimPool[counter]; //does this need to be global?
-			if(s.stimType.Equals(type) && s.category.Equals(cat))
-			{
-				if(s.hasBeenTarget)
-				{
-					targetCount++; //make an independent variable for this
-					if(targetCount == stimPool.Count)
-					{
-						break;
-					}
-					else{
-						counter++;
-						continue;
-					}
-				}
-				
-				// this block of code handles retrieving a
-				// stimulus to be the target
-				float f = Random.Range (0.0f,4.0f);
-				if (f < 1.0f) {
-					stimPool[counter].hasBeenTarget = true;
-					s.isCorrect = true;
-					answer.Add(s); //pass this, return this
-					host.setHostMedia(s); //global
-					break;
-				}
-			}
-			counter++;
-		}
-		counter = 0;
-		while (answer.Count < 4) {
+			//do revolution counting in a separate function
 			if (counter == stimPool.Count)
 			{
-				counter = 0;
-				revolutionCount++;
-				if(revolutionCount > 5)
-				{
-					resetTargets();
-					revolutionCount = 0;
-				}
+				counter = 0;	
 			}
-			serStim s = stimPool[counter];
-			if(s.hasBeenTarget)
-			{
-				targetCount++;
-				if(targetCount == stimPool.Count)
-				{
-					Debug.Log("ran out of " + diffLevel + " stims");
-					ScoreTracker inst = GameObject.Find ("Main Camera").GetComponent<ScoreTracker>();
-					diffLevel = inst.resetDifficulty(diffLevel); //if we run out of one difficulty don't freak out
-					resetTargets();
-					counter = 0;
-					revolutionCount++;
-					continue;
-				}
-				else{
-					counter++;
-					continue;
-				}
-			}
+			serStim s = stimPool[counter];	
 			// when a list of 4 serStims is assembled, findStim returns answer
-			 if (s.stimType.Equals (type) && 
-			    s.difficulty.Equals (diffLevel) &&
-			    !answer.Contains(s) && s.category.Equals(cat)) { 
-				// this block of code handles generating non-target
-				// stimuli
-			    float f = Random.Range (0.0f,4.0f);
-				if (f < 1.0f) {
-					answer.Add(s);
-				}
-				 
+			 if (matchesCriteria (cat, diffLevel, type, s) && !answer.Contains(s)) 
+			{
+				answer = randomAdd (answer, s); 
 			}
 			counter++;
 		}
 		return answer;
 	}
 
+//********************************
+// HELPER FUNCTIONS FOR findStim *
+//********************************
+
+	//use the category to set the type of stimulus findStim will search for
+	string setType (Category cat)
+	{
+		string type;
+		if (cat.Equals (Category.ReceptiveVocabulary) || cat.Equals(Category.Customization)) 
+		{
+			type = "visual";
+		} else 
+		{
+			type = "audio";
+		}	
+		return type;
+	}
+	
+	//select the target stimulus for a given question, according to the given criteria
+	serStim selectTarget (Category cat, Difficulty diffLevel, string type)
+	{
+		serStim s = null;
+		int counter = 0;
+		bool foundTarget = false;
+		if (checkFreeStims (cat,diffLevel,type) == 0) //if all stimuli have been used as a target (should not happen with full complement of stims)
+		{
+			resetTargets();
+		}
+		while (foundTarget == false)
+		{
+			if (counter == stimPool.Count) 
+				counter = 0;
+			s = stimPool[counter]; 
+			if(s.stimType.Equals(type) && s.category.Equals(cat))
+			{
+				if(s.hasBeenTarget) //pass over any previously used targets
+				{
+					counter++;
+					continue;
+				}
+				// this block of code handles retrieving a
+				// stimulus to be the target
+				float f = Random.Range (0.0f,4.0f);
+				if (f < 1.0f) {
+					stimPool[counter].hasBeenTarget = true;
+					s.isCorrect = true;
+					foundTarget = true;
+					host.setHostMedia(s); // pass the target's prompt to the main character
+					break;
+				}
+			}
+			counter++;
+		}
+		return s;
+	}
+
+	//check for stimuli that correspond the to the given criteria, and have not been used as a target yet
+	int checkFreeStims (Category cat, Difficulty diffLevel, string type)
+	{	
+		int total = 0;
+		for(int i = 0; i< stimPool.Count; i++)
+		{
+			if (matchesCriteria(cat, diffLevel, type, stimPool[i]) && !stimPool[i].hasBeenTarget)
+				total++;
+		}
+		return total;
+	}
+
+	//check to make sure a stimulus matches the defined criteria
+	bool matchesCriteria (Category cat, Difficulty diff, string type, serStim s)
+	{
+		return (s.stimType.Equals (type) && 
+			s.difficulty.Equals (diff) &&
+				s.category.Equals(cat));
+	}	
+
+	//randomly choose whether or not to add the selected stimulus
+	List<serStim> randomAdd(List<serStim> answer, serStim s)
+	{
+		float f = Random.Range (0.0f,4.0f);
+		if (f < 1.0f) 
+		{
+			answer.Add(s);
+		}
+		return answer;
+	}
+
+	//reset targets when all have been used (should be obsolete with full complement of stimuli)
 	void resetTargets()
 	{
+	
 		for (int i = 0; i < stimPool.Count; i++)
 		{	
 			stimPool[i].hasBeenTarget = false;
@@ -224,7 +242,8 @@ public class SpawnerScript : MonoBehaviour {
 			q.init(questionNumber, host.getOptions(), host.getBodyPart());
 			return spawnSOO(q);	
 		}
-		else {
+		else 
+		{
 			List<serStim> temp = findStim(cat, difLevel);
 			q.init (questionNumber, temp, cat);
 			return spawnSOO(q);
@@ -268,20 +287,23 @@ public class SpawnerScript : MonoBehaviour {
 				newStims[i] = Instantiate (character) as GameObject; //use the secondary character
 				spacing = charStimSpacing;	
 			}
-			else{
+			else
+			{
 				newStims[i] = Instantiate (stimPrefab) as GameObject; //just display the stimulus as a sprite
 				if(q.getCat() == Category.Customization)
 				{
 					spacing = textureSpacing;
 				}
-				else{
+				else
+				{
 					spacing = visStimSpacing;
 				}
 			}
 			newStims[i].transform.SetParent(newSoo.transform);
 			
 			//set stimulus position within SOO
-			switch(i){
+			switch(i)
+			{
 			case 0: 
 				newStims[i].transform.position = newSoo.transform.position + new Vector3 (spacing.x, spacing.y, 0); //upper left
 				break;
@@ -311,7 +333,8 @@ public class SpawnerScript : MonoBehaviour {
 		{
 			newStims[i].GetComponent<StimulusScript>().setStim(q.getStim(i));
 		}
-		else {
+		else 
+		{
 			newStims[i].GetComponent<StimulusScript>().setStim(q.getStim(i));
 			newStims[i].GetComponent<StimulusScript>().initSprite();	
 		}		
@@ -336,7 +359,8 @@ public class SpawnerScript : MonoBehaviour {
 
 	bool needsCharacter (Category cat)
 	{
-		switch (cat){
+		switch (cat)
+		{
 		case Category.ReceptiveVocabulary: //add categories that don't need the SecChar as cases here
 			return false;
 		case Category.Customization:
